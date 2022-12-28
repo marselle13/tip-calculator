c3f98527fe09df26ed33d2e00da3958ab70f48d4
nika <nika.gogua8@gmail.com>
2022-12-28 20:27:19 +0400

    add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "strong-cyan": "hsl(172, 67%, 45%)",
+        "very-dark-cyan": "hsl(183, 100%, 15%)",
+        "dark-cyan": "hsl(186, 14%, 43%)",
+        "grayish-cyan": "hsl(184, 14%, 56%)",
+        "light-grayish-cyan": "hsl(185, 41%, 84%)",
+        "light-cyan": "hsl(189, 41%, 97%)",
+      },
+    },
   },
   plugins: [],
 };
